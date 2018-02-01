9bb4232675188136b9e6e49fdf35f1bcfa75364f
Antheus <dupartdimitri@gmail.com>
2018-02-01 10:21:41 +0100

    revamped

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -13,8 +13,9 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	{
 		if (!(newlst = ft_lstnew(lst->content, lst->content_size))
 			return (NULL);
-		ft_lstadd(&next, newlst);
 		newlst = f(lst);
+		ft_lstadd(&next, newlst);
+		next = next->next;
 		lst = lst->next;
 	}
 	return (next);
